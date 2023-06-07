1433e01c9a7f40ebf3a9355f78636a44e19ea939
yoyobabycool <121718122+yoyobabycool@users.noreply.github.com>
2023-06-07 19:07:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,6 @@ consumer_secret = os.environ['TWITTER_API_SECRET_KEY']
 access_token = os.environ['TWITTER_ACCESS_TOKEN']
 access_token_secret = os.environ['TWITTER_ACCESS_TOKEN_SECRET']
 
-print(f'keys:{consumer_key}, {consumer_secret}, {access_token}, {access_token_secret}')
-
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
