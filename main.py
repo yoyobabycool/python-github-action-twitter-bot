8042c779df23a79eea744aceab3cc954ed69e83c
chris <chunghao777@gmail.com>
2023-06-12 12:54:10 +0800

    multiple functions and add comfig.ini

--- main.py
+++ main.py
@@ -1,46 +1,73 @@
 import os
 import tweepy
-import datetime
 import json
 import requests
+from configparser import ConfigParser
 
-consumer_key = os.environ['TWITTER_API_KEY']
-consumer_secret = os.environ['TWITTER_API_SECRET_KEY']
-access_token = os.environ['TWITTER_ACCESS_TOKEN']
-access_token_secret = os.environ['TWITTER_ACCESS_TOKEN_SECRET']
+class TwitterBot:
+    def __init__(self, config_file='config.ini'):
+        self.load_config(config_file)
+        self.auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
+        self.auth.set_access_token(self.access_token, self.access_token_secret)
+        self.api = tweepy.API(self.auth)
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
-#api.update_status(status='test')
-#print("images tweeted")
-with open('quotes.json', 'r') as f:
-    quotes = json.load(f)
-    
-# Load last tweeted quote index from a file
-if os.path.exists('last_tweet_index.txt'):
-    with open('last_tweet_index.txt', 'r') as f:
-        tweet_index = int(f.read().strip())
-else:
-    tweet_index = -1
+    def load_config(self, config_file):
+        # Load API keys and other configurations from the config file
+        config = ConfigParser()
+        config.read(config_file)
 
-print(f'index : {tweet_index}')
-# Get the quote to tweet
-quote = quotes[tweet_index]
-# Compose the tweet text
-tweet_text = f'"{quote["quote"]}" - {quote["character"]}'
-image_url = quote['image']
-filename = f"image.jpg"
-image = requests.get(image_url).content
-with open(filename, "wb") as f:
-    f.write(image)
-#media_upload = api.media_upload(filename)
-#tweet_media_id = media_upload.media_id
-#api.update_status(status=tweet_text, media_ids=[tweet_media_id])
-api.update_status(status=tweet_text)
-print(f'Tweeted: {tweet_text}')
-next_tweet_index = (tweet_index + 1) 
-if next_tweet_index not in range(0, 15):
-     next_tweet_index = 0
-with open('last_tweet_index.txt', 'w') as f:
-    f.write(str(next_tweet_index))
+        self.consumer_key = config.get('Twitter', 'consumer_key')
+        self.consumer_secret = config.get('Twitter', 'consumer_secret')
+        self.access_token = config.get('Twitter', 'access_token')
+        self.access_token_secret = config.get('Twitter', 'access_token_secret')
+
+    def load_quotes(self, quotes_file='quotes.json'):
+        # Load the quotes list from the quotes file
+        with open(quotes_file, 'r') as f:
+            self.quotes = json.load(f)
+
+    def load_last_tweet_index(self, index_file='last_tweet_index.txt'):
+        # Load the last tweeted quote index from a file
+        if os.path.exists(index_file):
+            with open(index_file, 'r') as f:
+                self.tweet_index = int(f.read().strip())
+        else:
+            self.tweet_index = -1
+
+    def save_last_tweet_index(self, index_file='last_tweet_index.txt'):
+        # Save the next tweet index to a file
+        with open(index_file, 'w') as f:
+            f.write(str(self.next_tweet_index))
+
+    def get_next_tweet(self):
+        # Get the next quote to tweet
+        self.tweet_index = (self.tweet_index + 1) % len(self.quotes)
+        return self.quotes[self.tweet_index]
+
+    def download_image(self, url, filename):
+        # Download the image and save it to the specified file
+        image = requests.get(url).content
+        with open(filename, 'wb') as f:
+            f.write(image)
+
+    def tweet(self, quote, image_filename):
+        # Send a tweet
+        tweet_text = f'"{quote["quote"]}" - {quote["character"]}'
+        self.api.update_with_media(image_filename, tweet_text)
+
+    def run(self):
+        # Main execution function
+        self.load_quotes()
+        self.load_last_tweet_index()
+
+        quote = self.get_next_tweet()
+        image_filename = 'image.jpg'
+        self.download_image(quote['image'], image_filename)
+        self.tweet(quote, image_filename)
+
+        self.next_tweet_index = (self.tweet_index + 1) % len(self.quotes)
+        self.save_last_tweet_index()
+
+if __name__ == '__main__':
+    bot = TwitterBot()
+    bot.run()
