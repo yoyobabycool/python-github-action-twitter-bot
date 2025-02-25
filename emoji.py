37f0931b5ec2de5390c51e08b31c8742ea07e570
yoyobabycool <121718122+yoyobabycool@users.noreply.github.com>
2025-02-25 23:34:42 +0530

    change image path

--- emoji.py
+++ emoji.py
@@ -17,12 +17,12 @@ if url_response.status_code == 200:
   image_data = url_response.json()
   image_url = image_data.get("url")
   print(f"image url {image_url}")
-  image_path = os.path.join(os.getcwd(), "emoji_image.png")
+  image_path = f"emoji_image.png"
   print(f"image path {image_path}")
   if image_url:
     image_response = requests.get(image_url)
     if image_response.status_code == 200:
-      with open(image_path,"rb") as file:
+      with open(image_path,"wb") as file:
         media_id = client.media_upload(file=file).media_id_string
         tweet_text = 'check this!'
         client.create_tweet(text = tweet_text, media_ids=[media_id])
